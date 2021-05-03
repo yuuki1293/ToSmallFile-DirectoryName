d2fc6250e6aa01321e5164543cad04beccef3340
yuuki1293 <yuuki1293@outlook.jp>
2021-05-03 09:57:53 +0900

    first

---
+++ Program.fs
@@ -0,0 +1,29 @@
+// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open System.IO
+
+let targetPath = Console.ReadLine()
+
+let ToAbsolutelyPath relativePath = Path.Combine(targetPath, relativePath)
+
+let ToRelativePath (absolutelyPath: string) =
+    let parentPathLength = targetPath.Length
+
+    let relativePath =
+        absolutelyPath.[(parentPathLength + 1)..(absolutelyPath.Length - 1)]
+
+    relativePath
+
+let ToSmallPath (path: string) =
+    let relativePath = path |> ToRelativePath
+    let newPath = relativePath.ToLower()
+    let newAbsolutelyPath = ToAbsolutelyPath newPath
+    newAbsolutelyPath
+
+let ToSmallFileAndDirectory path =
+    match path |> File.GetAttributes with
+    | FileAttributes.Directory -> Directory.Move(path, ToSmallPath path)
+    | _ -> File.Move(path, ToSmallPath path)
+
+ignore (Console.ReadKey())
